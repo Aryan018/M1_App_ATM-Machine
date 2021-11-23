179e5e8a95897b9a33e952f3867fa8f6febb5060
Aryan Saxena <60808641+Aryan018@users.noreply.github.com>
2021-11-23 15:57:41 +0530

    Delete exit_fun.c

--- 3_Implementation/MultiFile/exit_fun.c
+++
@@ -1,7 +0,0 @@
-#include "fun.h"
-void menuExit() {
-    printf("--------------Take your receipt!!!------------------\n");
-    printf("-----Thank you for using ATM Banking Machine!!!-----\n");
-
-
-}//exit menu
