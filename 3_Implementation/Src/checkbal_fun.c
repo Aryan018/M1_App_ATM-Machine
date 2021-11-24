ec81d964bc730d689757832d61f9158cf2302387
Aryan Saxena <60808641+Aryan018@users.noreply.github.com>
2021-11-24 23:50:30 +0530

    Update checkbal_fun.c

--- 3_Implementation/Src/checkbal_fun.c
+++ 3_Implementation/Src/checkbal_fun.c
@@ -1,4 +1,4 @@
-#include "fun.h"
+#include "../Inc/fun.h"
 void checkBalance(float balance) {
     printf("You Choose to See your Balance\n");
     printf("\n\n****Your Available Balance is:   Rs%.2f\n\n", balance);
